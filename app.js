d8dbce0d79444495e3fb05f0bf914d16f14a271d
Titouan-Loeb <titouan.loeb@epitech.eu>
2023-11-05 23:21:44 +0100

    feat(add_param_to_buffer): require owner param in url for the form

--- app.js
+++ app.js
@@ -1,50 +1,55 @@
 require("dotenv").config();
-const express = require('express');
+const express = require("express");
 const app = express();
-const { EventEmitter } = require('events')
-const bodyParser = require('body-parser');
-const saveCharacter = require('./public/js/components/form.js');
-const { connect, connection } = require('mongoose');
+const { EventEmitter } = require("events");
+const bodyParser = require("body-parser");
+const saveCharacter = require("./public/js/components/form.js");
+const { connect, connection } = require("mongoose");
 const { db } = require("./public/js/schema/characters.schema.js");
-const database_token = process.env.DATABASE_TOKEN
+const database_token = process.env.DATABASE_TOKEN;
 
 /* ---------- SETUP ---------- */
-app.use(express.static('public'));
-app.use('/css', express.static(__dirname + 'public/css'));
-app.use('/js', express.static(__dirname + 'public/js'));
+app.use(express.static("public"));
+app.use("/css", express.static(__dirname + "public/css"));
+app.use("/js", express.static(__dirname + "public/js"));
 
 // set views
-app.set('views', './views');
-app.set('view engine', 'ejs');
+app.set("views", "./views");
+app.set("view engine", "ejs");
 
 connect(database_token).catch(console.error);
 
 /* ---------- EVENTS ---------- */
 const dbEvents = connection;
-dbEvents.on('error', (error) => {
-    console.error('Database connection error:', error);
-})
-dbEvents.once('open', () => {
-    console.log('Database connected successfully');
-})
+dbEvents.on("error", (error) => {
+  console.error("Database connection error:", error);
+});
+dbEvents.once("open", () => {
+  console.log("Database connected successfully");
+});
 
 /* ---------- ROUTES ---------- */
 app.use(bodyParser.urlencoded({ extended: true }));
 // index
-app.get('', (req, res) => {
-    res.render('index');
+app.get("", (req, res) => {
+  res.render("index");
 });
 
 // form
-app.get('/form', (req, res) => {
-    res.render('form');
+app.get("/form", (req, res) => {
+  const { owner } = req.query;
+  if (owner === undefined) {
+    res.render("form/errorPath");
+		return;
+  }
+  res.render("form/form", { owner });
 });
-app.post('/form', (req, res) => {
-    const formData = req.body;
-    saveCharacter(formData);
+app.post("/form", (req, res) => {
+  const formData = req.body;
+  saveCharacter(req, res);
 });
 
 /* ---------- LISTEN ---------- */
 app.listen(3000, () => {
-    console.log('Server is listening on port 3000');
+  console.log("Server is listening on port 3000");
 });
